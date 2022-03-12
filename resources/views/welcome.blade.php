45d36a666db9f03c666f6d74b4b23d28e8ec7b00
mnsry <mansory.eca@gmail.com>
2022-03-13 01:14:15 +0330

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <div class="main-container container pt-80 pb-80" id="main-container">
         <!-- post content -->
         <div class="blog__content mb-72">
-            <div class="container text-center">
+            <div class="container">
                 <img src="{{ Voyager::Image(setting('site.home_banner')) }}" alt="">
                 <br><br>
                 <h1>خوش آمدید</h1>
